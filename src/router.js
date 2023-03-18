f10f7615bd0828fc69f7081ca85b29e606ce1d7c
mantou <mantou001@qq.com>
2023-03-18 11:37:20 +0800

    学习了解栅格系统响应式布局

--- src/router.js
+++ src/router.js
@@ -5,11 +5,18 @@ const Login = () => import(/* webpackChunkName: 'login' */ './views/Login.vue')
 const Reg = () => import(/* webpackChunkName: 'reg' */ './views/Reg.vue')
 const Forget = () =>
   import(/* webpackChunkName: 'forget' */ './views/Forget.vue')
+const Home = () =>
+  import(/* webpackChunkName: 'forget' */ './views/Home.vue')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Home
+    },
     {
       path: '/login',
       name: 'login',
