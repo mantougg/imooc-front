1b25f689790a9d90a454086ea7f50a9e96460116
mantou <mantou001@qq.com>
2023-03-25 22:28:07 +0800

    优化登录路由

--- src/store.js
+++ src/store.js
@@ -6,11 +6,20 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     sid: '',
-    isLogin: false
+    isLogin: false,
+    userInfo: {}
   },
   mutations: {
     setSid (state, value) {
       state.sid = value
+    },
+    // 设置用户的基本信息
+    setUserInfo (state, value) {
+      state.userInfo = value
+    },
+    // 设置用户的登录状态
+    setIsLogin (state, value) {
+      state.isLogin = value
     }
   },
   actions: {
